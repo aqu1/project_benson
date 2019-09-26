7524f2659c939aecb1af35bea8be46a35d22da86
Dan Roth <rdan689@gmail.com>
2019-09-25 22:03:40 -0400

    checkpoint code and data submit by Dan

--- python_code/mta_locate.py
+++ python_code/mta_locate.py
@@ -1,40 +1,13 @@
-import numpy as np
-from geopy.geocoders import GoogleV3, Nominatim
-import re
 import pandas as pd
-import googlemaps
 import requests
-import gmaps
-import gmaps.datasets
-from random import sample
 
 
-def geolocate(s, api_key):
-    geolocator = GoogleV3(api_key=api_key)
-    location = geolocator.geocode(s)
-    return [location.latitude, location.longitude]
-    # heatmaps
-
-
-def revlocate(search, api_key):
-    geolocator = GoogleV3(api_key=api_key)
-    location = geolocator.reverse(search)
-    print(location.raw)
-
-
-def zipcode(search, api_key):
-    geolocater = Nominatim(user_agent="metis")
-    location = geolocater.geocode(search)
-    zipcode = re.search(r'\d{5}(?:-\d{4})?(?=\D*$)', location.address).group()
-    return zipcode
-
-
-def geoGoogle(search, key, info='postcode'):
+def geoGoogle(search, key, infolist):
+    """Function to receive and address' geocode data from Google Maps."""
     # Set up your Geocoding url
     geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address={}".format(search)
     if key is not None:
         geocode_url = geocode_url + "&key={}".format(key)
-
     # Ping google for the results:
     results = requests.get(geocode_url)
     # Results will be in JSON format - convert to dict using requests functionality
@@ -50,54 +23,64 @@ def geoGoogle(search, key, info='postcode'):
         "postcode": ",".join([x['long_name'] for x in answer.get('address_components')
                               if 'postal_code' in x.get('types')])
     }
-    return output[info]
+    return [output[info] for info in infolist]
 
 
-def heatmap(df, key):
-    location = []
-    for index in df.STATION.sample(n=50, random_state=76).index:
-        location.append(tuple([df.STATION[index], df.ENTRIES[index]]))  # replace with lat and long
-    # Use google maps api
-    gmaps.configure(api_key=key)  # Fill in with your API key
-    # Get the dataset
-    earthquake_df = gmaps.datasets.load_dataset_as_df('earthquakes')
-    # Get the locations from the data set
-    locations = earthquake_df[['latitude', 'longitude']]
-    # Get the magnitude from the data
-    weights = earthquake_df['magnitude']
-    # Set up your map
-    fig = gmaps.figure()
-    fig.add_layer(gmaps.heatmap_layer(locations, weights=weights))
-    return fig, locations, weights
-
-
-def df_locate(df, key):
+def df_locate(addresses, key):
+    """Function to iterate over a set of addresses using geoGoogle function."""
     stations = []
-    lats = []
-    longs = []
-    for entry in df:
-        print(entry)
-        stations.append(entry)
-        lats.append(geoGoogle(entry, key, info='latitude'))
-        longs.append(geoGoogle(entry, key, info='longitude'))
-    d = {'stationline': stations, 'latitude': lats, 'longitude': longs}
+    coord = []
+    bors = []
+    for address in addresses:
+        stations.append(address)
+        coord.append(geoGoogle(address, key, ['latitude', 'longitude']))
+        # bors.append(geoGoogle(entry, key, info='formatted_address').split(',')[1])
+        """use the 'formatted_address' attribute of geoGoogle output for search for borough substrings
+        maybe use regex, wasn't sure here"""
+    d = {'stationline': stations, 'Latitude': list(zip(*coord))[0], 'Longitude': list(zip(*coord))[1]}
     locate_df = pd.DataFrame(d)
     return locate_df
 
 
-def main():
-    key = 'AIzaSyCxLwaGqiRunNaJ0Uvt93GH_RVd-S-2hIw'
-    turnstile_df = pd.read_csv('mta-3months.txt', low_memory=False)
-    turnstile_df = turnstile_df\
+def process_mta(csv, key):
+    """Function to process MTA turnstile dataset using the df_locate function.  Creates a location dataframe that is
+    then merged with the original input dataframe."""
+    turnstile_df = pd.read_csv(csv, low_memory=False)
+    print('CSV imported')
+    turnstile_df.LINENAME = turnstile_df.LINENAME.str \
+        .strip('W') \
+        .replace('LNQR456', '456LNQR') \
+        .replace('ABCD1', '1ABCD') \
+        .replace('FLM123', '123FLM') \
+        .replace('BDNQR2345', '2345BDNQR') \
+        .replace('R2345', '2345R') \
+        .replace('ACJZ2345', '2345ACJZ') \
+        .replace('ACENQRS1237', '1237ACENQRS')
+    turnstile_df = turnstile_df \
         .assign(stationline=turnstile_df.STATION.astype(str) + ' ' + turnstile_df.LINENAME.astype(str) + ' station NYC')
-
-    subways = ['14 ST-UNION SQ LNQR456W station NYC']
-    for subway in subways:
-        print(geoGoogle(subway, key, info='latitude'))
+    print('DF strings prepped')
     stations = sorted(list(set(turnstile_df.stationline)))
-    # station_locate = df_locate(stations, key)
-    # station_locate.to_csv('station_locations.csv')
-    # faster to do each unique value and map?  faster to do per unique station then merge.
+    print('Start location calculations')
+    location_df = df_locate(stations, key)
+    print('Location calculations successful')
+    location_df = location_df.assign(LINENAME=location_df.stationline.apply(lambda x: ''.join(x.split()[2])))
+    location_df.stationline = location_df.stationline.apply(lambda x: ' '.join(x.split()[:-3]))
+    location_df.rename(columns={'stationline': 'STATION'}, inplace=True)
+    final_df = pd.merge(turnstile_df, location_df, left_on=['STATION', 'LINENAME'], right_on=['STATION', 'LINENAME'])
+    final_df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    print('DF processed and merged')
+    return final_df
+
+
+def main():
+    """essential variables here"""
+    key = 'AIzaSyCxLwaGqiRunNaJ0Uvt93GH_RVd-S-2hIw'
+    csv = '../data/turnstiles_daily_filtered.csv'
+    outcsv = 'brooklyn_turnstiles.csv'
+
+    """execute functions"""
+    final_df = process_mta(csv, key)
+    final_df.to_csv(outcsv)
 
 
 if __name__ == '__main__':
